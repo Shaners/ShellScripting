a153ca88e21cfbd6a507bd1e012ad08a21068891
Shane Lister <shane@atramentous.com>
2013-03-20 13:24:08 -0700

    Update Record-DailyTemps.ps1

--- Record-DailyTemps.ps1
+++ Record-DailyTemps.ps1
@@ -1,8 +1,22 @@
-# temp func
+function Get-CurrentTemp {
+    $ErrorActionPreference = "SilentlyContinue"
+    $WeatherXML = ( New-Object System.Net.WebClient ).DownloadString( "http://rss.wunderground.com/auto/rss_full/global/stations/71201.xml?units=metric" )
+    if ( $? ) {
+        ( $WeatherXML -replace "`n", " " ) -match '(Current Conditions.*?(Temperature: ).*?(?<CurrentTemp>\d+(\.\d+)?))' | out-null
+        if ( $? ) {
+            return [double]$Matches["CurrentTemp"]
+        }
+    }
+    $ErrorActionPreference = "Continue"
+}
 
 # read temperatures.csv to collection, if locked retry every 1 second forever, if csv not exist create empty collection
+$Temperatures = @( Import-CSV Temperatures.csv )
+$Temperatures = @( )
+# finish this sandwich
 
-# call get temp
+$CurrentTemp = Get-CurrentTemp
+
 
 # if today exists in collection change it's temp to current temp
 
