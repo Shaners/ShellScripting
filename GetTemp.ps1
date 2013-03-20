7b09c4f14bc8e84b237ddaa03e386a0740b26e6b
Shane Lister <shane@atramentous.com>
2013-03-20 12:27:48 -0700

    Update GetTemp.ps1

--- GetTemp.ps1
+++ GetTemp.ps1
@@ -1,7 +1,13 @@
 function Get-CurrentTemp {
+    $ErrorActionPreference = "SilentlyContinue"
     $WeatherXML = ( New-Object System.Net.WebClient ).DownloadString( "http://rss.wunderground.com/auto/rss_full/global/stations/71201.xml?units=metric" )
-    ( $WeatherXML -replace "`n", " " ) -match '(Current Conditions.*?(Temperature: ).*?(?<CurrentTemp>\d+(\.\d+)?))' | out-null
-    return [double]$Matches["CurrentTemp"]
+    if ( $? ) {
+        ( $WeatherXML -replace "`n", " " ) -match '(Current Conditions.*?(Temperature: ).*?(?<CurrentTemp>\d+(\.\d+)?))' | out-null
+        if ( $? ) {
+            return [double]$Matches["CurrentTemp"]
+        }
+    }
+    $ErrorActionPreference = "Continue"
 }
 
 Get-CurrentTemp
