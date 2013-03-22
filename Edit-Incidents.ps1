aac71c57c2adacbf5a959867539d3a4513067765
Shane Lister <shane@atramentous.com>
2013-03-22 15:15:26 -0700

    Update Edit-Incidents.ps1

--- Edit-Incidents.ps1
+++ Edit-Incidents.ps1
@@ -1,9 +1,37 @@
-# IsLocked func here
+function IsLocked {
+    param( [string]$FileName )
+    
+    $ErrorActionPreference = "SilentlyContinue"
+    $File = [IO.File]::OpenWrite( $FileName )
+    $Result = $?
+    $ErrorActionPreference = "Continue"
+    if ( $Result ) { $File.Close() }
+    return (-not $Result)
+}
 
-# check existence of "incidents*todaysdate*.docx"
-#   If not exist then create by copying from template file "Incidents_Template.docx"
+# Ensures location is the same as PS's working directory
+[System.IO.Directory]::SetCurrentDirectory($(get-location))
 
-# Test for if the file is locked with islocked func
+# Constructs file name
+$CurrentDate =  (Get-Date).ToString('yyyy-MM-dd')
+$FileName = "Incidents_$CurrentDate.docx"
+
+# Checks existence of file
+#   If not exist then create by copying from template
+if ( -not (test-path ".\$FileName" -pathtype leaf) ) {
+    Copy-Item ".\Incidents_Template.docx" -Destination ".\$Filename"
+}
+
+# Test if the file is locked with islocked func
 #   If locked wait for it to become unlocked and display a message that updates
+if ( IsLocked($FileName) ) {
+    write-host "Waiting for file to be unlocked"
+    do {
+        Start-Sleep -s 1
+        write-host -nonewline "." # padright to make this blink?
+    }
+    until ( -not ( IsLocked($FileName) ) )
+}
 
-# call invoke-item to edit file ???
+# Call invoke-item to edit file
+Invoke-Item ".\$FileName"
