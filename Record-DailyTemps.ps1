6613329bd79da2a0168a677fe2af70371353dae5
Shane Lister <shane@atramentous.com>
2013-03-22 12:36:52 -0700

    Update Record-DailyTemps.ps1

--- Record-DailyTemps.ps1
+++ Record-DailyTemps.ps1
@@ -10,45 +10,41 @@ function Get-CurrentTemp {
     $ErrorActionPreference = "Continue"
 }
 
-# read temperatures.csv to collection
-#   if csv not exist create empty collection
-#   if locked retry every 1 second forever
+# Reads temperatures.csv into a collection
+#   If csv does not exist, will create empty collection
+#   If locked will retry opening every 1 second forever
 if ( -not (test-path ".\Temperatures.csv" -pathtype leaf) ) {
-    write-host "Temperatures.csv does not exist" # remove this later
     $Temperatures = @( )
 }
 else {
     do {
-        Start-Sleep -s 5 # change to 1 after working
+        Start-Sleep -s 1
         $ErrorActionPreference = "SilentlyContinue"
-        Import-CSV Temperatures.csv | out-null # dirty? seems to work
+        Import-CSV Temperatures.csv | out-null # dirty? is there a better way?
         $Result = $?
         $ErrorActionPreference = "Continue"
-        write-host "Result = $Result" # remove this later
     }
     until ( $Result )
     $Temperatures = @( Import-CSV Temperatures.csv )
 }
 
-write-host "Loop has ended. Result is $Result" # remove this later
-write-host $Temperatures # remove this later
-
 $CurrentTemp = Get-CurrentTemp
 $CurrentDate = (Get-Date).ToString('yyyy-MM-dd')
 
-# if today exists in collection change it's temp to current temp
-if ( $Temperatures.date -contains $CurrentDate ) {
-    foreach ( $record in $Temperatures ) { # Got to be a better way to do this
-        if ( $record.date -eq $CurrentDate ) {
-            $record.temp = $CurrentTemp
+# If today exists in collection change it's temp to current temp
+if ( $Temperatures -match $CurrentDate ) { # This will fail if it checks against an object like produced in else statement
+    foreach ( $Record in $Temperatures ) { # Got to be a better way to do this
+        if ( $Record.Date -eq $CurrentDate ) {
+            $Record.Temp = $CurrentTemp
         }
     }
-} # if today not exist in collection add current date plus current temp
+} # If today does not exist in collection add current date plus current temp as a new object to collection
 else {
-    $Temperatures.Add($CurrentDate","$CurrentTemp) # Will this work??
-    # $Temperatures +=(($CurrentDate),($CurrentTemp)) # Alternative method
+    $TodayDateTemp = New-Object Object
+    $TodayDateTemp | Add-Member NoteProperty "Date" $CurrentDate
+    $TodayDateTemp | Add-Member NoteProperty "Temp" $CurrentTemp
+    $Temperatures += $TodayDateTemp
 }
-# Needs testing
 
 # write collection to temperatures.csv using safe update strat ( rename orginal .bak, new .new then .csv )
 # if temperatures locked retry every 1 second forever
