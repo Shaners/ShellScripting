73ec1718af3f250c38c020c3bfaee1a7d87323a7
Shane Lister <shane@atramentous.com>
2013-03-22 13:51:59 -0700

    Create Edit-Incidents.ps1

---
+++ Edit-Incidents.ps1
@@ -0,0 +1,9 @@
+# IsLocked func here
+
+# check existence of "incidents*todaysdate*.docx"
+#   If not exist then create by copying from template file "Incidents_Template.docx"
+
+# Test for if the file is locked with islocked func
+#   If locked wait for it to become unlocked and display a message that updates
+
+# call invoke-item to edit file ???
