4abdb6c588a2c2ac515811a8e5359478e9444053
Shane Lister <shane@atramentous.com>
2013-04-03 22:57:13 -0700

    Create StudySheet.py

---
+++ StudySheet.py
@@ -0,0 +1,44 @@
+# Powershell cmdlets
+# Which cmdlets perform which tasks
+
+# How cmdlet parameters work
+
+# How to read help syntax descriptions
+
+
+# Powershell pipelines
+# How to write a series of cmlets in a pipeline 
+
+# Stages of 'get', 'filter', 'process' and 'formatting' cmlets in a pipeline
+
+# Use of parenthesis to include pipeline results in a statement
+
+
+# PS variables and Operators
+# Which operators do what
+
+# Data types and their use
+
+# Collections
+
+# Special variable names used by powershell
+
+# String quote rules
+
+# Variable and expression substition in strings
+
+# Data type methods, .ToString .SubString and etc
+
+
+# Statements, functions, error handling
+# Syntax and operations of statements like if, switch, for etc
+
+# Writing functions, using function arguments, returning values from functions
+
+# How to surpress error messages and check if a statement has failed
+
+
+# Regular expressions
+
+
+# Write a simple function and script that uses that function
